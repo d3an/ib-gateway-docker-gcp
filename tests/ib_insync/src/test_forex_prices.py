362490a67e1502e021d6e8d6a7ffe2caecbf87be
James Bury <19847770+d3an@users.noreply.github.com>
2021-12-22 22:33:26 -0500

    Update test_forex_prices.py

--- tests/ib_insync/src/test_forex_prices.py
+++ tests/ib_insync/src/test_forex_prices.py
@@ -1,24 +1,18 @@
 import logging
 import os
-import ib_insync
-import sys
 import random
+import sys
 
-loglevel = 10 # Set module-wide loglevel here
+import ib_insync as ibis
 
-""" Begin setting module-wide parameters"""
-# Log for ib_insync
-ib_insync.util.logToConsole(level=10)
-# Initialise Logging class
-logging.basicConfig()
-logging.getLogger().setLevel(loglevel)
-logging.info("Reporting INFO-level messages")
-""" End setting module-wide parameters"""
+
+# Set module-wide log level here
+LOG_LEVEL = logging.INFO
 
 
 def connect_to_ib():
     # Connect to market gateway
-    ibinstance = ib_insync.IB()
+    ibinstance = ibis.IB()
     ibinstance.connect(host=os.getenv('IB_GATEWAY_PAPER_URLNAME', 'tws-paper'),
                        port=int(os.getenv('IB_GATEWAY_PAPER_URLPORT', '4004')),
                        clientId=int(os.getenv('EFP_CLIENT_ID', (5+random.randint(0, 4)))),
@@ -48,7 +42,7 @@ def get_forex_pairs(ib_conn):
     :return:
     """
     currency_pairs = ["USDAUD", "USDGBP", "USDEUR"]
-    contracts = [ib_insync.Forex(pair) for pair in currency_pairs]
+    contracts = [ibis.Forex(pair) for pair in currency_pairs]
     ib_conn.qualifyContracts(*contracts)
     return contracts
 
@@ -77,7 +71,7 @@ def start_ticker_feed(contracts, ib_conn):
         for tick in tickers:
             # We can't test the below unless we are within 24/5 trading!
             if tick.midpoint() > 0:
-                single_tick = ib_insync.util.tree(tick)['Ticker']
+                single_tick = ibis.util.tree(tick)['Ticker']
                 consume_tickers(single_tick)
 
     def onTimeoutOccuring(time_since_event):
@@ -88,12 +82,19 @@ def start_ticker_feed(contracts, ib_conn):
     ib_conn.pendingTickersEvent += onPendingTickers
     ib_conn.timeoutEvent += onTimeoutOccuring
     # Start the timeout after the tickers have been requested
-    ib_insync.util.sleep(10)  # Give IB 5 seconds grace to start giving us tickers
+    ibis.util.sleep(10)  # Give IB 5 seconds grace to start giving us tickers
     ib_conn.setTimeout(20)
 
 
 def main():
-    ib_insync.util.sleep(90)
+    logging.basicConfig()
+    logging.getLogger().setLevel(LOG_LEVEL)
+    logging.info("Reporting INFO-level messages")
+    
+    ibis.util.sleep(90)
+    ibis.util.startLoop()
+    ibis.util.logToConsole(level=LOG_LEVEL)
+    
     ib_conn = connect_to_ib()
     logging.info("test_forex_prices connected to IB")
     contracts = get_forex_pairs(ib_conn)
@@ -102,4 +103,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
